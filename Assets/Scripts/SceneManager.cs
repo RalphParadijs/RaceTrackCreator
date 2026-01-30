99123d5aca713cb0962e2890cc234372beaed4a9
rvanderneut1 <r.vanderneut1@avans.nl>
2026-01-30 14:14:07 +0100

    Add project files.

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject MainScreen;
+    [SerializeField] private GameObject RaceCreatorScreen;
+    
+    public void ShowHideMainScreen()
+    {
+        MainScreen.SetActive(!MainScreen.activeSelf);
+    }
+    public void ShowHideRaceCreatorScreen()
+    {
+        RaceCreatorScreen.SetActive(!RaceCreatorScreen.activeSelf);
+    }
+
+
+}
