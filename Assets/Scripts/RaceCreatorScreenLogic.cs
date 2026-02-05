e9f3976aebdaa6345237177506693a2d47bab990
rvanderneut1 <r.vanderneut1@avans.nl>
2026-02-05 10:46:21 +0100

    added script

--- Assets/Scripts/RaceCreatorScreenLogic.cs
+++ Assets/Scripts/RaceCreatorScreenLogic.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class RaceCreatorScreenLogic : MonoBehaviour
 {
     public GameObject CreateTrackManager;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void OnEnabled()
+    
+    //OnEnabled is 
+    void OnEnabled()
     {
         var tracks = CreateTrackManager.GetComponent<CreateTrackManager>().tracks;
         for (int i = 0; i <= tracks.Count; i++)
